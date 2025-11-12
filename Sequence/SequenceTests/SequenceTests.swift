99fa7433dcb405c78ebcfa3240d01dbbfb6921cc
AD <silentflute.ad@gmail.com>
2025-11-12 14:04:06 +0000

    Wrote initial models and initial test Adding mock up images for reference

--- Sequence/SequenceTests/SequenceTests.swift
+++ Sequence/SequenceTests/SequenceTests.swift
@@ -6,30 +6,26 @@
 //
 
 import XCTest
+@testable import Sequence
 
 final class SequenceTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testHashing() {
+       let pos1 = GridPosition(row: 2, col: 3)
+       let pos2 = GridPosition(row: 2, col: 3)
+       
+       var set = Set<GridPosition>()
+       set.insert(pos1)
+       set.insert(pos2)
+       
+       XCTAssertEqual(set.count, 1, "Identical positions should hash to same value")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
