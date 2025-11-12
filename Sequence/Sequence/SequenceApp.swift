d793aa0e8c2c65a4eebfb8758cd5f25786039b8e
AD <silentflute.ad@gmail.com>
2025-11-12 13:36:34 +0000

    Initial project build

---
+++ Sequence/Sequence/SequenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SequenceApp.swift
+//  Sequence
+//
+//  Created by Amarjit on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SequenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
