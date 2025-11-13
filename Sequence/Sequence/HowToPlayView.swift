a9403462ebd73e08644e0c2dfd2de7984a01b8ff
AD <silentflute.ad@gmail.com>
2025-11-13 16:39:22 +0000

    Adding how to play view - TBC

---
+++ Sequence/Sequence/HowToPlayView.swift
@@ -0,0 +1,19 @@
+//
+//  HowToPlayView.swift
+//  Sequence
+//
+//  Created by Amarjit on 13/11/2025.
+//
+
+import SwiftUI
+
+struct HowToPlayView: View {
+    
+    var body: some View {
+        Text("How to play")
+    }
+}
+
+#Preview {
+    HowToPlayView()
+}
